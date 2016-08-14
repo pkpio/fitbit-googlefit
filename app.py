ab6cb3abd5e0d8682c37da18e976db5f19969cd5
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-08-14 03:20:40 +0200

    Steps exporter in its 'raw' form

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import dateutil.tz
 import dateutil.parser
 import configparser
 import json
+from datetime import timedelta, date
 
 import fitbit
 from apiclient.discovery import build
@@ -57,13 +58,24 @@ def GetGoogleClient(filepath):
 	logging.debug("Google client created")
 	return client
 
-def GetDataSource():
-	"""Returns a data source for Google Fit data logging"""
+def GetDataSource(type='steps'):
+	"""Returns a data source for Google Fit data logging
+	
+	type - type of data. Possible options: steps, weight, heart_rate
+	"""
+	if type == 'steps':
+		dataType=dict(name='com.google.step_count.delta',field=[dict(name='steps',format='integer')])
+	elif type == 'weight':
+		dataType=dict(name='com.google.weight',field=[dict(format='floatPoint', name='weight')])
+	elif type == 'heart_rate':
+		dataType=dict(name='com.google.heart_rate.bpm',field=[dict(format='floatPoint', name='bpm')])
+	else:
+		raise ValueError("Unexpected data type given!")
+
 	return dict(
 		type='raw',
 		application=dict(name='fbit-gfit'),
-		dataType=dict(name='com.google.step_count.delta',
-			field=[dict(name='steps',format='integer')]),
+		dataType=dataType,
 		device=dict(type='watch',manufacturer='fitbit',model='charge-hr',
 			uid='io.pkp.fbit-gfit',version='1'))
 
@@ -82,6 +94,30 @@ def GetDataSourceId(dataSource,credsFilepath):
 		dataSource['device']['model'],
 		dataSource['device']['uid']))
 
+dawnOfTime = datetime.datetime(1970, 1, 1, tzinfo=dateutil.tz.tzutc())
+
+def epochOfFitbitLog(date, dataPoint, tzinfo):
+	logTimestamp = "{} {}".format(date, dataPoint['time'])
+	logTime = dateutil.parser.parse(logTimestamp).replace(tzinfo=tzinfo)
+	return (logTime - dawnOfTime).total_seconds()
+
+def nano(val):
+	"""Converts a number to nano (str)."""
+	return '%d' % (val * 1e9)
+
+def FitbitStepsToGoogleSteps(date, steps_point, tzinfo):
+	logSecs = epochOfFitbitLog(date, steps_point, tzinfo)
+
+	return dict(
+		dataTypeName='com.google.step_count.delta',
+		endTimeNanos=int(nano(logSecs))+110,
+		startTimeNanos=nano(logSecs),
+		value=[dict(intVal=steps_point['value'])]
+		)
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
 
 #======================== Main application code =========================
 def main():
@@ -92,21 +128,67 @@ def main():
 	parser.add_argument("-f", "--fitbit-creds", default="auth/fitbit.json", help="Fitbit credentials file")
 	args = parser.parse_args()
 
-	# Init client objects and setup Google Fit data sources
+	# Init client objects and setup Google Fit data sources for each type
 	fitbitClient,fitbitCreds = GetFitbitClient(args.fitbit_creds)
 	googleClient = GetGoogleClient(args.google_creds)
-	dataSourceId = GetDataSourceId(GetDataSource(),args.google_creds)
-	try:
-		googleClient.users().dataSources().get(userId='me',dataSourceId=dataSourceId).execute()
-	except HttpError as error:
-		if not 'DataSourceId not found' in str(error):
-			raise error
-		# Data source doesn't already exist so, create it!
-		googleClient.users().dataSources().create(userId='me',body=GetDataSource()).execute()
+	for dataType in ['steps', 'weight', 'heart_rate']:
+		dataSource = GetDataSource(dataType)
+		dataSourceId = GetDataSourceId(dataSource,args.google_creds)
+		try:
+			googleClient.users().dataSources().get(userId='me',dataSourceId=dataSourceId).execute()
+		except HttpError as error:
+			if not 'DataSourceId not found' in str(error):
+				raise error
+			# Data source doesn't already exist so, create it!
+			googleClient.users().dataSources().create(userId='me',body=dataSource).execute()
+	dataSourceIdSteps = GetDataSourceId(GetDataSource('steps'),args.google_creds)
+	dataSourceIdWeight = GetDataSourceId(GetDataSource('weight'),args.google_creds)
+	dataSourceIdHR = GetDataSourceId(GetDataSource('heart_rate'),args.google_creds)
 
-	# Testing
-	print(googleClient.users().dataSources().list(userId='me').execute())
-	print(fitbit_client.intraday_time_series('activities/steps'))
+	# Get user's time zone info from Fitbit -- since Fitbit time stamps are not epoch
+	userProfile = fitbitClient.user_profile_get()
+	tzinfo = dateutil.tz.gettz(userProfile['user']['timezone'])
+
+	# Iterate over a range of dates
+	start_date = date(2015, 5, 17)
+	end_date = date(2016, 8, 14)
+
+	for single_date in daterange(start_date, end_date):
+		day_stamp = single_date.strftime("%Y-%m-%d")
+
+	    # Start getting data from fitbit
+		interday_raw = fitbitClient.intraday_time_series('activities/steps',base_date=day_stamp,detail_level='1min')
+		steps_day = interday_raw['activities-steps'][0]['dateTime']
+		steps_data = interday_raw['activities-steps-intraday']['dataset']
+
+		# Probably not required - we do it on daily basis!
+		steps_data_times = [epochOfFitbitLog(steps_day, log, tzinfo) for log in steps_data]
+		minLogNs = nano(min(steps_data_times))
+		maxLogNs = int(nano(max(steps_data_times)))+200
+
+
+		googleStepPoints = [FitbitStepsToGoogleSteps(steps_day, log, tzinfo) for log in steps_data]
+		datasetId = '%s-%s' % (minLogNs, maxLogNs)
+
+		# Write a day of fitbit data
+		"""
+		print(googleClient.users().dataSources().datasets().get(
+		userId='me',
+		datasetId=datasetId,
+		dataSourceId=dataSourceIdSteps
+		).execute())
+		"""
+		googleClient.users().dataSources().datasets().patch(
+				userId='me',
+				dataSourceId=dataSourceIdSteps,
+				datasetId=datasetId,
+				body=dict(
+					dataSourceId=dataSourceIdSteps,
+					maxEndTimeNs=maxLogNs,
+					minStartTimeNs=minLogNs,
+					point=googleStepPoints)
+		).execute()
+		print("Synced for day : %s"%(steps_day))
 
 if __name__ == '__main__':
 	main()
