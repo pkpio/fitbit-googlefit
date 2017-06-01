57d0fa6ba4087e603d257f40164bee4c32c87e66
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-06-01 14:40:03 +0200

    headless authentication for google

--- auth/auth_google.py
+++ auth/auth_google.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 import sys
+import argparse
 
 from oauth2client.file import Storage
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run_flow, argparser
 
 def main():
-  client_id = sys.argv[1]
-  client_secret = sys.argv[2]
+    # Arguments parsing
+    parser = argparse.ArgumentParser("Client ID and Secret are mandatory arguments")
+    parser.add_argument("-i", "--id", required=True, help="Client id")
+    parser.add_argument("-s", "--secret", required=True, help="Client secret")
+    parser.add_argument("-c", "--headless", default=False, 
+        help="Authenticate only using console (for headless systems)", action="store_true")
+    args = parser.parse_args()
 
-  # Scopes of authorization
-  activity = "https://www.googleapis.com/auth/fitness.activity.write"
-  body = "https://www.googleapis.com/auth/fitness.body.write"
-  location = "https://www.googleapis.com/auth/fitness.location.write"
-  scopes = activity + " " + body + " " + location
+    # Scopes of authorization
+    activity = "https://www.googleapis.com/auth/fitness.activity.write"
+    body = "https://www.googleapis.com/auth/fitness.body.write"
+    location = "https://www.googleapis.com/auth/fitness.location.write"
+    scopes = activity + " " + body + " " + location
 
-  flow = OAuth2WebServerFlow(client_id, client_secret, scopes)
-  storage = Storage('google.json')
-  flags = argparser.parse_args([])
-  run_flow(flow, storage, flags)
+    flow = OAuth2WebServerFlow(args.id, args.secret, scopes)
+    storage = Storage('google.json')
+    flags = ['--noauth_local_webserver'] if args.headless else []
+    run_flow(flow, storage, argparser.parse_args(flags))
 
 if __name__ == '__main__':
-  main()
+    main()
