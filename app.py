6e6fe31ae90ff4b38b030fd0f05b0f3ae5a94f02
lightmaster <lightmaster.tech@gmail.com>
2017-12-07 03:47:01 -0500

    fix calling auth scripts using subprocess.call now

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Main class / entry point for the application
 __author__ = "Praveen Kumar Pendyala"
 __email__ = "mail@pkp.io"
 """
-import time,argparse,logging,datetime,dateutil.parser,configparser,json,os
+import time,argparse,logging,datetime,dateutil.parser,configparser,json,os,subprocess
 from datetime import timedelta, date
 from helpers import *
 from convertors import *
@@ -82,14 +82,14 @@ def main():
 		print("""\n===========================================================================\n===========================================================================\n\nGo to this site and register a new Fitbit app\n https://dev.fitbit.com/apps/new \n\n\nApplication Name :              --Choose a name--\nDescription :                   --Choose a description--\nApplication Website :           --Your website--\nOrganization :                  --Choose an organization--\nOrganization Website :          --Your website--\nOAuth 2.0 Application Type :    **Must choose 'Personal'**\nCallback URL :                  http://localhost:8080/ \nDefault Access Type :           Read-Only\n\nNote :\n1. Use your own information for fields marked --\n2. Make sure you copy the Callback URL exactly (including the last /)\n3. Application Type MUST be Personal\n\nMake a note of your 'OAuth 2.0 Client ID' and 'Client Secret'\n===========================================================================\n===========================================================================\n""")
 		sleep(2)
 		# prompt if on headless or browser
-		isbrowser = get_bool("Does this system have a native display and a browser?")
+		isbrowser = get_bool("Does this system have a native display and a browser? ")
 		fitbitclientid = input("What's your Fitbit Client ID? ")
 		fitbitclientsecret = input("What's your Fitbit Client Secret? ")
 		# run auth/auth_fitbit.py
 		if isbrowser == True:
-			auth_fitbit.main("-i " + fitbitclientid + " -s " + fitbitclientsecret)
+			subprocess.call("cd ./auth && ./auth_fitbit.py -i " + fitbitclientid + " -s " + fitbitclientsecret)
 		else:
-			auth_fitbit.main("-i " + fitbitclientid + " -s " + fitbitclientsecret + " --console")
+			subprocess.call("cd ./auth && ./auth_fitbit.py -i " + fitbitclientid + " -s " + fitbitclientsecret + " --console")
 	# if auth/google.json doesn't exist
 	if googleauthfile.is_file() == False:
 		print("""\n\n===========================================================================\n===========================================================================\n\nGo to https://console.developers.google.com/flows/enableapi?apiid=fitness\n\n1. Click 'Continue'. Then select 'Go to credentials' and select 'Client ID'.\n2. Under 'Application type', select 'Other' and hit 'Create'.\n3. Make a note of 'Client ID' and 'Client Secret'\n\n===========================================================================\n===========================================================================\n""")
@@ -98,14 +98,14 @@ def main():
 		try:
 			isbrowser
 		except NameError:
-			isbrowser = get_bool("Does this system have a native display and a browser?")
+			isbrowser = get_bool("Does this system have a native display and a browser? ")
 		googleclientid = input("What's your Google Client ID? ")
 		googleclientsecret = input("What's your Google Client Secret? ")
 		# run auth/auth_google.py
 		if isbrowser == True:
-			auth_google.main("-i " + googleclientid + " -s " + googleclientsecret)
+			subprocess.call("cd ./auth && ./auth_google.py -i " + googleclientid + " -s " + googleclientsecret)
 		else:
-			auth_google.main("-i " + googleclientid + " -s " + googleclientsecret + " --console")
+			subprocess.call("cd ./auth && ./auth_google.py -i " + googleclientid + " -s " + googleclientsecret + " --console")
 
 
 	# Reading configuration from config file
